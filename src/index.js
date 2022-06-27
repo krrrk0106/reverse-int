138c14d75a6a1f088d5b0b424c36adda841798b0
Yelizaveta Petrenko <p.yelizaveta.98@gmail.com>
2022-06-27 17:07:09 +0200

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let toString = Math.abs(n).toString();
+    let i = (toString.length - 1);
+    let reverseNum = '';
+    while (i >= 0) {
+      reverseNum = reverseNum + toString[i];
+      i--;
+    }
+    return reverseNum;
 }
